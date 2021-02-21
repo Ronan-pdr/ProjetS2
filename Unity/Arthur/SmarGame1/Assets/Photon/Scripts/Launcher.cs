82d430763c46544e82a5c4dc1bce7e5fce2551a9
Arthur Barbier <arthur.barbier@epita.fr>
2021-02-21 16:06:54 +0100

    Quelques changements mineurs

--- Unity/Arthur/SmarGame1/Assets/Photon/Scripts/Launcher.cs
+++ Unity/Arthur/SmarGame1/Assets/Photon/Scripts/Launcher.cs
@@ -15,6 +15,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     [SerializeField] Transform playerListContent;
     [SerializeField] GameObject roomListItemPrefab;
     [SerializeField] GameObject PlayerListItemPrefab;
+    [SerializeField] GameObject startGameButton;
 
     void Awake()
     {
@@ -30,6 +31,7 @@ public class Launcher : MonoBehaviourPunCallbacks
     {
         Debug.Log("Connected to Master");
         PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
     }
 
     public override void OnJoinedLobby()
@@ -53,16 +55,32 @@ public class Launcher : MonoBehaviourPunCallbacks
         MenuManager.Instance.OpenMenu("room");
         roomNameText.text = PhotonNetwork.CurrentRoom.Name;
         Player[] players = PhotonNetwork.PlayerList;
+        foreach (Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
         for (int i = 0; i < players.Length; i++)
         {
             Instantiate(PlayerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
         }
+
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.SetActive(PhotonNetwork.IsMasterClient);
     }
     public override void OnCreateRoomFailed(short returnCode, string message)
     {
         errorText.text = "Room Creation Failed" + message;
         MenuManager.Instance.OpenMenu("error");
     }
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
 
     public void LeaveRoom()
     {
@@ -89,6 +107,8 @@ public class Launcher : MonoBehaviourPunCallbacks
         }
         for (int i = 0; i < roomList.Count;i++)
         {
+            if (roomList[i].RemovedFromList)
+                continue;
             Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
         }
     }
