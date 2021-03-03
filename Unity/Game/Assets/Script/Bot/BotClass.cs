2b3deb3d92041c556c30874437e8e2f3c0de5317
Ronan Pédron <ronan.pedron@epita.fr>
2021-03-03 17:28:54 +0100

    création de la branche dépot et elle est toute clean

--- Unity/Game/Assets/Script/Bot/BotClass.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class BotClass : Humanoide
-{
-    protected override void SearchAnimation(string touche)
-    {}
-}
